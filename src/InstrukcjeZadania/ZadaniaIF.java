e57ded723eb1c35213289b87fac57f135ce6f7d9
Mikołaj <mikolajjanusz23@gmail.com>
2021-04-26 01:12:26 +0200

    #1 Dodano Gui + moduły do zadań

---
+++ src/InstrukcjeZadania/ZadaniaIF.java
@@ -0,0 +1,13 @@
+package InstrukcjeZadania;
+
+public class ZadaniaIF {
+	
+	public ZadaniaIF() {
+		zadanie1();
+	}
+	
+	public void zadanie1() {
+		System.out.println("Tresc zadania 1");
+	}
+	
+}
