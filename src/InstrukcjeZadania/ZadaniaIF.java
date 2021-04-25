c63c4143b01113b73d84082cfbf58a75ddaae2d4
Mikołaj <mikolajjanusz23@gmail.com>
2021-04-26 01:37:22 +0200

    completed task on if Instruction

--- src/InstrukcjeZadania/ZadaniaIF.java
+++ src/InstrukcjeZadania/ZadaniaIF.java
@@ -7,7 +7,19 @@ public class ZadaniaIF {
 	}
 	
 	public void zadanie1() {
-		System.out.println("Tresc zadania 1");
-	}
-	
+		System.out.println("======= Zadanie1 =======");
+		
+		for(int i=0; i<5; i++) {
+			int liczba = (int) Math.floor(Math.random()*20+1);
+			String odpowiedz;
+			
+			if(liczba*2+4 < 8)
+				odpowiedz = "Tak";
+			else
+				odpowiedz = "Nie";
+			System.out.println("\s" + "Liczba: " + liczba +" " + odpowiedz);
+		}
+		
+		System.out.println("========================");
+	}	
 }
